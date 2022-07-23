5443593294a8ea4c40bcb73b83874deb9971dac0
Maximiliano González <108374329+MaxiG-dev@users.noreply.github.com>
2022-07-23 20:50:19 -0300

    Update

--- 26 Web administrar citas/js/app.js
+++ 26 Web administrar citas/js/app.js
@@ -1,1 +1,26 @@
- 
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+const formulario = document.querySelector('#nueva-cita');
+const contenedorCitas = document.querySelector('#citas');
+
+eventListeners();
+function eventListeners() {
+    mascotaInput.addEventListener('input', datosCita);
+}
+
+const citaObj = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora: '',
+    sintomas: '',
+}
+
+function datosCita(e) {
+    console.log(e.target.value);
+}
